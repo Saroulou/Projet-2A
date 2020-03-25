f95180ea1be9e9a41b3cb3ca6baf3204d80b2df2
Malcolm <faribaultmalcolm@gmail.com>
2020-03-25 14:54:10 +0100

    ajout de la classe missile

---
+++ Missiles.java
@@ -0,0 +1,34 @@
+package jeuAvion;
+
+import java.awt.Color;
+import java.awt.Graphics;
+//import java.util.ArrayList;
+
+public class Missiles extends Avion { //extends Avion ?
+	
+
+	
+	public Missiles (double x, double y,double vr, double vtheta, int h, int l) {
+		super(x,y,vr,vtheta,h,l);
+		
+	}
+	
+	
+	public void dessine(Graphics g){
+
+		 int r = 20;
+	        g.setColor(Color.black);
+	        g.fillPolygon(
+	            new int[]{(int)(x+r*Math.cos(Math.toRadians(vtheta))),(int)(x+r*Math.cos(Math.toRadians(vtheta+120))),(int)(x+r*Math.cos(Math.toRadians(vtheta-120)))},
+	            new int[]{(int)(y+r*Math.sin(Math.toRadians(vtheta))),(int)(y+r*Math.sin(Math.toRadians(vtheta+120))),(int)(y+r*Math.sin(Math.toRadians(vtheta-120)))},
+	            3);
+    } 
+	
+	 public void avancer(){
+	        x=(x+vr*Math.cos(Math.toRadians(vtheta)));
+	        y=(y+vr*Math.sin(Math.toRadians(vtheta)));
+	 }
+	 
+
+
+}
