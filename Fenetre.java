413298fd08a07bf7a813de766d0c8b0859894df8
adam <adam.bakkali@insa-lyon.fr>
2020-04-08 16:05:05 +0200

    deplacement du Background

--- Fenetre.java
+++ Fenetre.java
@@ -12,46 +12,63 @@ import java.io.*;
 
 
 public class Fenetre extends JFrame implements ActionListener, KeyListener{
+	
+// ATTRIBUTS
 
     private ArrayList<Avion> avions;
     private Timer timer;
-    private final int var=100;
-    private BufferedImage image= null;
+    private final int var=10;
+    private ImageIcon sBackground; // permet de stocker l'image du fond d'écran
+	private Image iBackground; 
+	private final int largeurBackground = 1000;	
+	public int xBackground; // permet de determiner l'abcisse de l'image 
+	private int var1=0;
+	
+// CONSTRUCTEURS
 
     public Fenetre(String nom, int width, int height){
 
         super(nom);
-
+		this.sBackground = new ImageIcon(getClass().getResource("Background.jpg"));
+		this.iBackground = this.sBackground.getImage();
+		this.xBackground=0;
+		
+		
+		
         setSize(width, height);
         setLocation(200,200);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(null);
-
+		
         timer=new Timer(var, this);
         timer.start();
 
         avions=new ArrayList<Avion>();
         avions.add(new Avion(getHeight(),getWidth()));
 
-        try {
-            image = ImageIO.read(new File("Background.jpg"));
-        }
-        catch (IOException e){
-            e.printStackTrace();
-        }
-
         addKeyListener(this);
 
         setVisible(true);
     }
 
+// METHODES
+
     public Fenetre(){
-        this("Skywar",1000,700);
+        this("Skywar",1500,700);
     }
+    // permet de dessiner l'écran et de rajouter les images "à la suite".
+    
+    private void movingBackground(Graphics g){
+		if(this.xBackground == -this.largeurBackground){this.xBackground = 0;}
+		g.drawImage(this.iBackground, this.xBackground, 0, null);
+		g.drawImage(this.iBackground, this.xBackground + this.largeurBackground, 0, null);
+		g.drawImage(this.iBackground, this.xBackground+ this.largeurBackground * 2, 0, null);
+		g.drawImage(this.iBackground, this.xBackground + this.largeurBackground * 3, 0, null);
+}
 
     public void paint (Graphics g){
-        g.drawImage(image,0,0,null);
-      g.setColor(new Color (100,20,70));
+       this.movingBackground(g);
+        g.setColor(new Color (100,20,70));
         //g.fillRect(0,0,this.getWidth(),this.getHeight());
         for(Avion av: avions){
             av.dessine(g);
@@ -69,9 +86,10 @@ public class Fenetre extends JFrame implements ActionListener, KeyListener{
     }
 
 
-
     public void actionPerformed(ActionEvent e){
-         for(Avion av: avions){
+		var1++;
+		if(var1%10==0){
+        for(Avion av: avions){
             av.avancer();
             for(Bombe b:av.listebombe){
                 if(b!=null){
@@ -90,7 +108,14 @@ public class Fenetre extends JFrame implements ActionListener, KeyListener{
 
             }
         }
-        repaint();
+        
+       
+	}
+        
+        
+		xBackground --; // on récupere xBackground - 1 
+		repaint(); // appel a la methode paint 
+		 
     }
 
     public void keyPressed(KeyEvent e){
