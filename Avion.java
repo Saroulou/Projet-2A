e03f22cb7d7c264de51b233720266d98cca28398
Sarobidy <sarobidy.andrianaivohasina@gmail.com>
2020-04-08 17:25:11 +0200

    Fixe bug bombe+trajectoire

--- Avion.java
+++ Avion.java
@@ -1,40 +1,35 @@
 import javax.swing.*;
-import java.awt.event.*; 
+import java.awt.event.*;
 import java.awt.*;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JButton;
 import javax.swing.JTextField;
 import java.util.ArrayList;
-import java.util.LinkedList; 
+import java.util.LinkedList;
 
-public class Avion {
-	protected double x; //position
-    protected double y;
-    protected double vr;//vitesse
-    protected double vtheta;//degres
-    private int h; //hauteur fenetre 
-    private int l;
+public class Avion extends Objet{
+
     protected int nbMissiles = 0;
     protected final int nbMaxMissiles = 5;
-    public ArrayList <Bombe> listebombe;
-    public LinkedList <Missiles> missiles; 
-	
+    public ArrayList<Bombe> listebombe;
+    public ArrayList<Missiles> missiles;
+     public ArrayList <Bombe> listebombesuppr;
+
     public Avion (double x, double y, double vr, double vtheta, int h, int l) {
-		this.x=x;
-        this.y=y;
-        this.vr=vr;
-        this.vtheta=vtheta;
-        this.h = h; 
-        this.l = l;
+        super(x, y, vr, vtheta, h, l);
+
         listebombe=new ArrayList<Bombe>();
-        //bombe.add(new Bombe(this.x, this.y,getHeight(),getWidth()));
-	}
-    
+        missiles=new ArrayList<Missiles>();
+        listebombesuppr=new ArrayList<>();
+
+        listebombe.add(new Bombe(this.x, this.y,h,l,this));
+    }
+
     public Avion(int h, int l){
         this(100,100,10,0,h,l);
     }
-    
+
     public void dessine(Graphics g){
         int r = 30;
         g.setColor(new Color (243,149,70));
@@ -42,33 +37,33 @@ public class Avion {
             new int[]{(int)(x+r*Math.cos(Math.toRadians(vtheta))),(int)(x+r*Math.cos(Math.toRadians(vtheta+120))),(int)(x+r*Math.cos(Math.toRadians(vtheta-120)))},
             new int[]{(int)(y+r*Math.sin(Math.toRadians(vtheta))),(int)(y+r*Math.sin(Math.toRadians(vtheta+120))),(int)(y+r*Math.sin(Math.toRadians(vtheta-120)))},
             3);
-        
+
     }
-    
+
     public void avancer (){
         x=(x+l+vr*Math.cos(Math.toRadians(vtheta)))%l;
         y=(y+h+vr*Math.sin(Math.toRadians(vtheta)))%h;
-        
+
     }
-    
+
     public void tourner(double theta){
         vtheta+=theta;
-        
+
     }
-    
+
     public void tirerBombe(){
-        Bombe bombe=new Bombe(this.x, this.y,h,l);
+        Bombe bombe=new Bombe(this.x, this.y,h,l,this);
         listebombe.add(bombe);
     }
-    
+
     public void tirerMissiles() {
-    	
-    	if(nbMissiles<nbMaxMissiles) {
-    	Missiles missile = new Missiles(x,y,2*vr,2*vtheta,h,l);
-    	missiles.add(missile);
-    	nbMissiles++;
-    	}
-    	
+
+        if(nbMissiles<nbMaxMissiles) {
+            Missiles missile = new Missiles(x,y,2*vr,vtheta,h,l);
+            missiles.add(missile);
+            nbMissiles++;
+        }
+
     }
 
 }
--- Bombe.java
+++ Bombe.java
@@ -16,13 +16,19 @@ public class Bombe {
         public final double vitesse=10;
         public final double acceleration=9.8;
         public double r;
+        public Avion avion;
+		private final double vrInit;
+		private final double vthetaInit;
     
-    public Bombe (double x, double y,double h, double l){
+    public Bombe (double x, double y,double h, double l, Avion avion){
         this.x=x;
         this.y=y;
         this.h=h;
         this.l=l;
         this.r=20;
+        this.avion=avion;
+		this.vrInit=avion.vr;
+		this.vthetaInit=avion.vtheta;
     }
     
     public void dessine(Graphics g){
@@ -32,7 +38,10 @@ public class Bombe {
     }
     
     public void tombe(){
-        y=y+4.9;
+		//x=vrInit*Math.cos(Math.toRadians(vthetaInit))*Math.sqrt(2*(double)avion.h/acceleration);
+		x+=vrInit*Math.cos(Math.toRadians(vthetaInit));
+        y+=acceleration/2+vrInit*Math.abs(Math.sin(Math.toRadians(vthetaInit)));
+		//y=acceleration/2*Math.pow(x/Math.sin(Math.toRadians(vthetaInit)),2)-(double)avion.h;
     }
     
     public boolean estsorti (){
