a59ef7d25f8ae8955f5c4af04fc9274d04ae6bcf
Sarobidy <sarobidy.andrianaivohasina@gmail.com>
2020-05-06 14:30:18 +0200

    Fix merge

--- Main.java
+++ Main.java
@@ -1,10 +1,6 @@
 public class Main {
 	
-<<<<<<< HEAD
 	public static void main (String[] args) {     
-=======
-	public static void main (String[] args) {
->>>>>>> master
 		Fenetre fenetre=new Fenetre();
 	}
 }
